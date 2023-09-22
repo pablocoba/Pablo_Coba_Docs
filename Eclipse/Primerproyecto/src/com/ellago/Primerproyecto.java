885967f2635ee389830d23b505f06c6bdacce7be
Pablo Coba <contact.p4ul@gmail.com>
2023-09-22 14:06:34 +0200

    22/09/2023
    Proyecto de FOL añadido y ECLIPSE

---
+++ Eclipse/Primerproyecto/src/com/ellago/Primerproyecto.java
@@ -0,0 +1,30 @@
+
+package com.ellago;
+
+public class Primerproyecto {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("buenas!!!!");
+		int x = 5;
+		int y = 6;
+
+		System.out.println(x + y);
+
+		// declarar variables
+		byte mes = 12;
+		int contador = 0;
+		final double pi = 3.1415926535897932384626433832795;
+		float interes = 4.25e2F;
+		char letra = 'Z';
+		boolean encontrado = true;
+		// imprimir valores
+		System.out.println(mes); // imprimirá 12
+		System.out.println(contador); // imprimirá 0
+		System.out.println(pi); // imprimirá 3.141592653589793
+		System.out.println(interes); // imprimirá 425.0
+		System.out.println(letra); // imprimirá Z
+		System.out.println(encontrado); // imprimirá true
+	}
+
+}
---
+++ prueba/prueba.java
@@ -0,0 +1,10 @@
+public class prueba {
+
+	public static void main(String[] args) {
+		
+		System.out.println("buenas!!!! holaaaaaaaaaaa");
+        System.out.print("klk manito klk")
+		
+	}
+
+}
